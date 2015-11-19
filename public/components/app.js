0e4d15e0eecc9ae916fd33a528569d71b9b56c6a
Jorn Palsenbarg <jorn.palsenbarg@gmail.com>
2015-11-19 13:01:37 +0100

    Final boilerplate client

--- public/components/app.js
+++ public/components/app.js
@@ -1,18 +1,13 @@
-"use strict";
+(function () {
+    'use strict';
 
-var jarvisApp = angular.module('jarvisApp', [
-    'ui.router',
-    'jarvis'
-]);
+    var jarvisApp = angular.module('jarvisApp', [
+        'ui.router',
+        'jarvis'
+    ]);
 
-jarvisApp.config(["$stateProvider","$urlRouterProvider", function($stateProvider, $urlRouterProvider){
-    //$urlRouterProvider.otherwise("/devices");
-    //$stateProvider
-    //    .state('devices', {
-    //        url: "/devices",
-    //        templateUrl: "components/devices/devices.html",
-    //        controller: 'DevicesCtrl',
-    //        controllerAs: 'dc',
-    //        data: { pageTitle: "Devices" }
-    //    })
-}]);
+    jarvisApp.config(["$stateProvider","$urlRouterProvider", function($stateProvider, $urlRouterProvider){
+        $urlRouterProvider.otherwise("/");
+    }]);
+
+}());
