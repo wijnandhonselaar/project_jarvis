6a2ffde20da177406af65828c835141b12c34d80
Nicky Romeijn <nickyromeijn@Nickys-MacBook-Pro.local>
2015-11-23 09:54:49 +0100

    Initial commit

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var udpListener = require('./classes/udpListener');
 server.listen(80);
 
 app.use(express.static('public'));
 app.get('/', function (req, res) {
     res.sendfile(__dirname+'/public/index.html');
 });
-
