eeb7a403223c02e3b7162d6ed7394223f850f3ce
Esperk <esper_kuijs@hotmail.com>
2015-11-25 10:53:51 +0100

    models update, moved sok data to model object

--- models/actuator.js
+++ models/actuator.js
@@ -3,32 +3,34 @@ var type = thinky.type;
 
 var Actuator = thinky.createModel("Actuator", {
 	id: type.number().required(),
-	savedAt: type.date().default(thinky.r.now()),
-	name: type.string().required(),
-	sokVersion: type.number().required(),
-	description: type.string(),
-	commands: {
-		status: type.object().required().schema({
-			name: type.string().required(),
-			parameters: type.object(),
-			type: type.string(),
-			returns: type.any().required(),
-			description: type.string()
-		}),
-		on: type.object().required().schema({
-			name: type.string().required(),
-			parameters: type.object(),
-			type: type.string(),
-			returns: type.any().required(),
-			description: type.string()
-		}),
-		off: type.object().required().schema({
-			name: type.string().required(),
-			parameters: type.object(),
-			type: type.string(),
-			returns: type.any().required().required(),
-			description: type.string()
-		})
+	model: {
+		savedAt: type.date().default(thinky.r.now()),
+		name: type.string().required(),
+		sokVersion: type.number().required(),
+		description: type.string(),
+		commands: {
+			status: type.object().required().schema({
+				name: type.string().required(),
+				parameters: type.object(),
+				type: type.string(),
+				returns: type.any().required(),
+				description: type.string()
+			}),
+			on: type.object().required().schema({
+				name: type.string().required(),
+				parameters: type.object(),
+				type: type.string(),
+				returns: type.any().required(),
+				description: type.string()
+			}),
+			off: type.object().required().schema({
+				name: type.string().required(),
+				parameters: type.object(),
+				type: type.string(),
+				returns: type.any().required().required(),
+				description: type.string()
+			})
+		}
 	},
 	config: {
 		ip: type.string().required(),
