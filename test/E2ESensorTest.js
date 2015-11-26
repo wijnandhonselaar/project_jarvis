a2f5b615c7ff2280cd419dd84b442802ef88e328
Jeroen Peters <jeroen-5@hotmail.com>
2015-11-26 14:27:48 +0100

    bugfixing mochserver + tests

--- test/E2ESensorTest.js
+++ test/E2ESensorTest.js
@@ -1,30 +1,30 @@
-var webdriverio =require('webdriverio');
-var expect = require('chai').expect;
+// var webdriverio =require('webdriverio');
+// var expect = require('chai').expect;
 
-describe('Test 1', function(){
-	this.timeout(10000);
-	var browser;
+// describe('Test 1', function(){
+// 	this.timeout(10000);
+// 	var browser;
 
-	before(function(done) {
-		browser = webdriverio.remote({
-			desiredCapabilities: {
-				browserName: 'phantomjs'
-			}
-		});
-		browser.init(done);
-	});
+// 	before(function(done) {
+// 		browser = webdriverio.remote({
+// 			desiredCapabilities: {
+// 				browserName: 'phantomjs'
+// 			}
+// 		});
+// 		browser.init(done);
+// 	});
 
-	it("Should read the news", function(done){
-		browser.
-			url("http://www.bbc.com/news/world-middle-east-34917485")
-			.getText(".story-body__h1").then( function(result){
-				console.log("Headline is: " + result);
-				expect(result).to.be.a("string");
-				done();
-			});
-	})
+// 	it("Should read the news", function(done){
+// 		browser.
+// 			url("http://www.bbc.com/news/world-middle-east-34917485")
+// 			.getText(".story-body__h1").then( function(result){
+// 				console.log("Headline is: " + result);
+// 				expect(result).to.be.a("string");
+// 				done();
+// 			});
+// 	})
 
-	after(function(done){
-		browser.end(done);
-	});
-});
+// 	after(function(done){
+// 		browser.end(done);
+// 	});
+// });
