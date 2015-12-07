f2e6018bd4a43a99f01f0c3a1f3048461eefe31b
Wijnand Honselaar <w.g.honselaar@gmail.com>
2015-12-07 12:35:27 +0100

    Added attributes to model

--- models/scenario.js
+++ models/scenario.js
@@ -2,7 +2,14 @@ var thinky = require('./thinky.js');
 var type = thinky.type;
 
 var Scenario = thinky.createModel("Scenario", {
-
+    name: type.string().required(),
+    description: type.string().required(),
+    actuators: [
+        {
+            deviceid: type.number().required(),
+            action: type.string().required()
+        }
+    ]
 });
 
 module.exports = Scenario;
