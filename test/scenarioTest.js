391cb4ac8069c15cca7fa27ec9cee99a963412a8
Wijnand Honselaar <w.g.honselaar@gmail.com>
2015-12-07 13:56:01 +0100

    Finished scenario test

--- test/scenarioTest.js
+++ test/scenarioTest.js
@@ -1,5 +1,5 @@
 var expect = require('chai').expect;
-var Scenario = require('../models/scenario.js');
+var Scenario = require('../models/scenario');
 
 describe('Scenario', function () {
     before(function(done) {
@@ -8,9 +8,9 @@ describe('Scenario', function () {
     });
     it('Shouldn\'t save an empty Scenario', function (done) {
         // create the new Scenario
-        var Scenario = new Scenario();
+        var scenario = new Scenario({});
         // save the new sensor
-        Scenario.save(Scenario).then(function(res) {
+        Scenario.save(scenario).then(function(res) {
             //done('saved with no status object');
         }).error(function(){
             done();
@@ -18,61 +18,61 @@ describe('Scenario', function () {
     });
     it('Shouldn\'t save a new Scenario, no description', function (done) {
         // create the new loh
-        var Scenario = new Scenario({
+        var scenario = new Scenario({
             name: 'name'
         });
         // save the new Scenario
-        Scenario.save(Scenario).then(function(res) {
+        Scenario.save(scenario).then(function(res) {
         }).error(function(){
             done();
         });
     });
     it('Shouldn\'t save a new Scenario, no name', function (done) {
         // create the new Scenario
-        var Scenario = new Scenario({
+        var scenario = new Scenario({
             description: 'description'
         });
         // save the new Scenario
-        Scenario.save(Scenario).then(function(res) {
+        Scenario.save(scenario).then(function(res) {
         }).error(function(){
             done();
         });
     });
     it('Shouldn\'t save a new Scenario, empty actuator object.', function (done) {
         // create the new Scenario
-        var Scenario = new Scenario({
+        var scenario = new Scenario({
             name: 'name',
             description: 'description',
             actuators: [{}]
         });
         // save the new Scenario
-        Scenario.save(Scenario).then(function(res) {
+        Scenario.save(scenario).then(function(res) {
         }).error(function(){
             done();
         });
     });
     it('Shouldn\'t save a new Scenario, no actuator action.', function (done) {
         // create the new Scenario
-        var Scenario = new Scenario({
+        var scenario = new Scenario({
             name: 'name',
             description: 'description',
             actuators: [{deviceid: 1}]
         });
         // save the new Scenario
-        Scenario.save(Scenario).then(function(res) {
+        Scenario.save(scenario).then(function(res) {
         }).error(function(){
             done();
         });
     });
     it('Shouldn\'t save a new Scenario, no actuator id.', function (done) {
         // create the new Scenario
-        var Scenario = new Scenario({
+        var scenario = new Scenario({
             name: 'name',
             description: 'description',
             actuators: [{action: "on"}]
         });
         // save the new Scenario
-        Scenario.save(Scenario).then(function(res) {
+        Scenario.save(scenario).then(function(res) {
         }).error(function(){
             done();
         });
@@ -80,12 +80,12 @@ describe('Scenario', function () {
 
     it('Should save a new Scenario', function (done) {
         // create the new Scenario
-        var Scenario = new Scenario({
+        var scenario = new Scenario({
             name: 'name',
             description: 'description',
         });
         // save the new Scenario
-        Scenario.save(Scenario).then(function(res) {
+        Scenario.save(scenario).then(function(res) {
             done();
         }).error(function(err){
             console.log(err);
